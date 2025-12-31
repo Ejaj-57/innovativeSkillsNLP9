9a54cde1fa71432e303bd74562748a6a15d861b6
K.M. Ejaj ul Ambia <k.m.ejajulambia@KMs-MacBook-Air.local>
2025-12-31 13:23:20 +0600

    updated class_04

---
+++ class_04/loops.py
@@ -0,0 +1,7 @@
+name = "k m ejaj ul ambia"
+for i in range(len(name)):
+    if name[i] == ' ':
+        continue
+# continue is used to skip the current iteration and move to the next iteration
+    print(name[i])
+
